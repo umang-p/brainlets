95830707d78635742fa25e453a7b612673e158a3
Umang Patel <patel.umang5@gmail.com>
2019-02-20 12:03:18 -0500

    Update equip timers page to not use gfdb data

--- brainlets/girlsfrontline/views.py
+++ brainlets/girlsfrontline/views.py
@@ -17,19 +17,17 @@ def sim(request):
     return render(request, 'girlsfrontline/sim.html')
 
 def equip_timers(request):
-    with open(settings.STATIC_ROOT+'/girlsfrontline/equip_info.json', mode='r') as data:
+    with open(settings.STATIC_ROOT+'/girlsfrontline/equips.json', encoding='utf-8', mode='r') as data:
         equip_info = json.load(data)
 
     equip_list = []
     for equip in equip_info:
-        if equip['fit_guns'] == '' and equip['cn_name'].find('16lab') == -1 and equip['cn_name'].find('16Lab') == -1 and equip['company'] != '' and (int(equip['max_level']) != 0 or int(equip['rank']) == 2) and int(equip['id']) != 51:
+        if equip['en_craftable'] == True:
             info = {}
-            info['hours'] = '{0:0>2.0f}'.format(int(equip['develop_duration']) / 60 // 60)
-            info['minutes'] = '{0:0>2.0f}'.format(int(equip['develop_duration']) / 60 % 60)
-            info['rarity'] = int(equip['rank'])
-            info['type'] = __get_equip_type(equip['type'], equip['cn_name'])
-            info['poseffect'] = __get_positive_effect(info['type'])
-            info['negeffect'] = __get_negative_effect(info['type'])
+            info['hours'] = '{0:0>2.0f}'.format(int(equip['construct_time']) / 60 // 60)
+            info['minutes'] = '{0:0>2.0f}'.format(int(equip['construct_time']) / 60 % 60)
+            info['rarity'] = int(equip['rarity'])
+            info['type'] = __get_equip_type(equip['type'])
             equip_list.append(info)
 
     equip_list = sorted(equip_list, key=itemgetter('hours','minutes'))
@@ -52,7 +50,7 @@ def doll_timers(request):
             doll_list.append(info)
 
     doll_list = sorted(doll_list, key=itemgetter('hours','minutes'))
-    
+
     return render(request, 'girlsfrontline/doll_timers.html', {"doll_list" : doll_list})
 
 def __get_doll_type(doll_type):
@@ -72,7 +70,7 @@ def __get_doll_type(doll_type):
     else:
         return 'error'
 
-def __get_equip_type(equip_type, name):
+def __get_equip_type(equip_type):
     equip_type = int(equip_type)
     if equip_type == 1:
         return 'scope'
@@ -87,92 +85,22 @@ def __get_equip_type(equip_type, name):
     elif equip_type == 6:
         return 'hp_ammo'
     elif equip_type == 7:
-        if name.find('#') == -1:
-            return 'slug'
-        else:
-            return 'buck'
+        return 'slug'
     elif equip_type == 8:
         return 'hv_ammo'
+    elif equip_type == 9:
+        return 'buck'
     elif equip_type == 10:
-        if name.find('T') == -1:
-            return 'x_exo'
-        else:
-            return 't_exo'
+        return 'x_exo'
     elif equip_type == 11:
         return 'armor'
+    elif equip_type == 12:
+        return 't_exo'
     elif equip_type == 13:
         return 'suppressor'
     elif equip_type == 14:
         return 'ammo_box'
     elif equip_type == 15:
         return 'cape'
-    else:
-        return 'error'
-
-def __get_positive_effect(equip_type):
-    if equip_type == 'scope':
-        return '+crit chance'
-    elif equip_type == 'EOT':
-        return '+accuracy +damage'
-    elif equip_type == 'red_dot_sight':
-        return '++acc'
-    elif equip_type == 'PEQ':
-        return '+night penalty reduction'
-    elif equip_type == 'ap_ammo':
-        return '+armor penetration'
-    elif equip_type == 'hp_ammo':
-        return '+damage'
-    elif equip_type == 'slug':
-        return '+damage +accuracy'
-    elif equip_type == 'buck':
-        return '+damage +crit damage'
-    elif equip_type == 'hv_ammo':
-        return '+damage'
-    elif equip_type == 'x_exo':
-        return '+evasion'
-    elif equip_type == 't_exo':
-        return '++evasion'
-    elif equip_type == 'armor':
-        return '+armor'
-    elif equip_type == 'suppressor':
-        return '+evasion +crit chance'
-    elif equip_type == 'ammo_box':
-        return '+clip size'
-    elif equip_type == 'cape':
-        return '+crit damage'
-    else:
-        return 'error'
-
-def __get_negative_effect(equip_type):
-    if equip_type == 'scope':
-        return 'none'
-    elif equip_type == 'EOT':
-        return '-rate of fire'
-    elif equip_type == 'red_dot_sight':
-        return '-rate of fire'
-    elif equip_type == 'PEQ':
-        return 'none'
-    elif equip_type == 'ap_ammo':
-        return 'none'
-    elif equip_type == 'hp_ammo':
-        return '-armor penetration'
-    elif equip_type == 'slug':
-        return '-targets hit'
-    elif equip_type == 'buck':
-        return 'none'
-    elif equip_type == 'hv_ammo':
-        return 'none'
-    elif equip_type == 'x_exo':
-        return 'none'
-    elif equip_type == 't_exo':
-        return '-damage'
-    elif equip_type == 'armor':
-        return '-evasion'
-    elif equip_type == 'suppressor':
-        return 'none'
-    elif equip_type == 'ammo_box':
-        return '-evasion'
-    elif equip_type == 'cape':
-        return '-movement speed'
     else:
         return 'error'
